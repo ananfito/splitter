8fa3d39d592e514dbd3430096beb03ac63919c72
Anthony Nanfito <81890820+ananfito@users.noreply.github.com>
2023-03-04 07:13:13 +0700

    update split function and refactor code
    removed unneeded functions, added notebook files for scratch work, and modified sample data file

--- splitter.py
+++ splitter.py
@@ -3,12 +3,12 @@
 
 # import libraries
 import pandas as pd
+import numpy as np
 from datetime import date
 
 # returns the current local date
 today = date.today()
 
-# FUNCTIONS
 # split(): separates rows of data frame into individual CSV files
 def split():
     print("Splitting files ...")
@@ -19,36 +19,6 @@ def split():
         i+=1
     print("Split complete.")
 
-# missingAssignmentReport(): generates list of missing assignments for each student
-def missingAssignmentReport():
-    print("Generating missing assignment reports ...")
-
-    i = 0
-    columnTitles = list(data) # creates array of column names
-    studentsMissingWork = {} # empty dictionary to store missing assignments
-
-    # loop through the rows and columns of the dataframe for any 'nan' entries
-    # 'nan' entries are missing assignments that students need to complete
-    # student names and list of missing assignment are stored in studentsMissingWork dictionary
-    while i < len(data):
-        j = 1
-        while j < len(columnTitles) - 1:
-            if data.iloc[i][columnTitles[j]].astype(str) == 'nan':
-                if data.iloc[i][columnTitles[0]] not in studentsMissingWork.keys():
-                    studentsMissingWork.update({data.iloc[i][columnTitles[0]]: [columnTitles[j]]})
-                else:
-                    studentsMissingWork.get(data.iloc[i][columnTitles[0]]).append(columnTitles[j])
-            j+=1
-        i+=1
-    
-    # iterate through the dictionary to create individual filees for students' missing work
-    for student in studentsMissingWork:
-        file = open('%s_%s.txt' % (today, student), 'w')
-        file.write('Below is a list of the assignment(s) you are missing. Please complete for credit, then email me me your list of completed assignment(s) with their new scores. Thank you. \n%s' % studentsMissingWork[student])
-        file.close()
-    
-    print("Reports generated.")
-
 # startup message
 print("Welcome to Splitter. \nDesigned by Anthony Nanfito. \nThis is a python script which splits the rows of CSV file and saves them as individual CSV files. Learn more at https://github.com/ananfito/splitter. \nFollow the instructions below to split your file.")
 
@@ -56,14 +26,6 @@ print("Welcome to Splitter. \nDesigned by Anthony Nanfito. \nThis is a python sc
 dataPath = input("Please type (or copy and paste) the file path to the CSV path your with to split: ")
 
 # reads CSV file and creates data frame
-data = pd.read_csv(dataPath)
+data = pd.read_csv(dataPath).replace(np.nan, "MISSING")
 
-# user input: how do you want to manipulate this file?
-command = input("How do you want to manipulate this file? \nPlease select the option you want by typing the correspond number followed by the 'Enter' key:\n 1: Split each of selected file into individual files.\n 2: Generate missing assignment report\n 0: Quit program\n")
-
-if command == '0':
-    quit()
-elif command == '1':
-    split()
-elif command == '2':
-    missingAssignmentReport()
+split()
